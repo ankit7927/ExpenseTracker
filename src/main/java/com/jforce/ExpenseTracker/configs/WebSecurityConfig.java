0cac80430dd0c9ee85c7e9f60924768f68281759
ankit <ap8424856@gmail.com>
2024-08-10 16:47:43 +0530

    allowed cors to all route

--- src/main/java/com/jforce/ExpenseTracker/configs/WebSecurityConfig.java
+++ src/main/java/com/jforce/ExpenseTracker/configs/WebSecurityConfig.java
@@ -21,6 +21,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -81,9 +83,9 @@ public class WebSecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-//        configuration.setAllowedOrigins(List.of("*"));
-//        configuration.setAllowedMethods(List.of("GET","POST", "PUT", "PATCH", "DELETE"));
-//        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET","POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
